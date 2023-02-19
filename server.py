a04f9a3b0f29fbd0bba4471f5f12ffff29593727
TabarakoAkula <mishabor7@mail.ru>
2023-02-19 15:36:18 +0300

    Yes

--- server.py
+++ server.py
@@ -35,5 +35,37 @@ def astronaut_selection():
     return render_template('astronaut_selection/index.html')
 
 
+@app.route('/choice/<planet_name>')
+def planet(planet_name):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предложение: {planet_name}</h1>
+                    <h3>Эта планета близка к Земле</h3>
+                    <div class="alert alert-success" role="alert">
+                  <h5>На ней много деревьев(было)</h5>
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                  <h5>С водой наблюдаются проблемы</h5>
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                  <h5>Полетим же на нее</h5>
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                  <h5>Ведь она такая красивая</h5>
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
